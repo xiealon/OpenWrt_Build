59ef0f09ee96cfce0de4da511324e9fd32e9dd05
username-OSS <1578465250@qq.com>
2025-05-03 20:01:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -95,6 +95,7 @@ sed -i '$i uci set network.wan6.ifname=""' ${defaultsettings}/files/zzz-default-
 sed -i '$i uci set network.lan.ipaddr="10.10.10.220"' ${defaultsettings}/files/zzz-default-settings
 sed -i '$i uci set network.lan.gateway="10.10.10.10"' ${defaultsettings}/files/zzz-default-settings
 sed -i '$i uci set network.lan.dns="10.10.10.10 , 112.112.208.1 , 139.9.23.90 , 180.76.76.76 , 223.5.5.5 , 223.6.6.6 , 8.8.8.8 ,8.8.4.4"' ${defaultsettings}/files/zzz-default-settings
+sed -i '$i uci set dhcp.lan.ignore="1"' ${defaultsettings}/files/zzz-default-settings
 
 #sed -i '$i uci set network.wan.proto=pppoe' ${defaultsettings}/files/zzz-default-settings
 #sed -i '$i uci set network.wan6.ifname="eth0"' ${defaultsettings}/files/zzz-default-settings
