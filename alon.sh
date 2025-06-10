5bbab7c46b598dbc04d53e5677c97f36bb8d6dd4
Alon <1578465250@qq.com>
2025-06-10 18:29:17 +0800

    Update alon.sh

--- alon.sh
+++ alon.sh
@@ -65,9 +65,9 @@ else
 fi
 
 # 移除不需要的包
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
+# rm -rf feeds/luci/applications/luci-app-mosdns
+# rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+# rm -rf feeds/packages/utils/v2dat
 # rm -rf feeds/packages/lang/golang
 
 # 克隆新的 golang 包
