2e9ce81ab562dd8614e53c9799201fab3101feba
Alon <1578465250@qq.com>
2025-06-24 01:01:29 +0800

    Update alon.sh

--- alon.sh
+++ alon.sh
@@ -1,5 +1,5 @@
 # !/bin/base 
-sed i "/src-git alon /d; 1 src-git alon https://github.com/xiealon/openwrt-packages;lede" feeds.conf.default
+sed -i "/src-git alon /d; 1 src-git alon https://github.com/xiealon/openwrt-packages;lede" feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 ./scripts/feeds uninstall $(grep Package ./feeds/alon.index | awk -F: ':' '(print $2)')
