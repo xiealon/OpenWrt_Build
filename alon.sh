0933a38f8d346746b62463553630cb48a721e824
Alon <1578465250@qq.com>
2025-06-26 10:33:57 +0800

    Update alon.sh

--- alon.sh
+++ alon.sh
@@ -78,20 +78,20 @@ pkg_manager_cmd() {
             if [[ "$SYSTEM_TYPE" == "openwrt" ]]; then
                 "${PKG_MGR}" update -a
             else
-                "${PKG_MGR}" update -y
+                sudo "${PKG_MGR}" update -y
             fi ;;
         "install")
             shift
             if [[ "$SYSTEM_TYPE" == "openwrt" ]]; then
                 "${PKG_MGR}" install -a "$@"
             else
-                "${PKG_MGR}" install -y "$@"
+                sudo "${PKG_MGR}" install -y "$@"
             fi ;;
         "list")
             if [[ "$SYSTEM_TYPE" == "openwrt" ]]; then
                 "${PKG_MGR}" list | awk '{print $1}'
             else
-                "${PKG_MGR}" list --installed
+                sudo "${PKG_MGR}" list --installed
             fi ;;
     esac
 }
