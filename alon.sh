e7486714793e7d60f8d80f2d6f1341a2563f56c9
Alon <1578465250@qq.com>
2025-06-26 10:25:25 +0800

    Update alon.sh

--- alon.sh
+++ alon.sh
@@ -32,7 +32,7 @@ declare -A REPO_DEFINITIONS=(
 # ["alon-ubuntu"]="https://ubuntu.prod.repo/ubuntu focal main restricted universe"  ## 镜像URL 发行版代号 组件列表 [用空格隔开]
 # ["alon-centos"]="https://centos.prod.repo/centos/7/os/x86-64/"                    ## 基础镜像URL
 
-SOURCE_PRIORITY=("alon" "alon1" "alon2" "alon3")
+SOURCE_PRIORITY=( "alon" "alon1" "alon2" "alon3" )
 INSTALL_PACKAGES=()
 MAX_RETRY_LEVEL=3
 
