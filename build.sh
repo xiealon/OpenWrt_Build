2585d10c0cc916a73bb4431f987398b831dd6e07
Alon <1578465250@qq.com>
2025-06-29 02:40:17 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -32,9 +32,9 @@ CONFIG_OWNER="${CONFIG_ARRAY[1]}"
 CONFIG_ARCH="${CONFIG_ARRAY[2]}"
 
 if [ "${CONFIG_REPO}" = "lede" ]; then
-#   REPO_URL="https://github.com/xiealon/openwrt-wolf"
-#   REPO_BRANCH="master"
-# elif [ "${CONFIG_REPO}" = "lede" ]; then
+  REPO_URL="https://github.com/xiealon/openwrt-wolf"
+  REPO_BRANCH="master"
+elif [ "${CONFIG_REPO}" = "lede" ]; then
   REPO_URL="https://github.com/xiealon/lede"
   REPO_BRANCH="master"
 else
