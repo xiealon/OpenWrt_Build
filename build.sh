e09f00f731e8c5cb8323f548dde7e0f41c1d9574
Alon <1578465250@qq.com>
2025-07-10 08:33:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ WORK_PATH="$(pwd)"
 
 SCRIPT_FILE="${WORK_PATH}/diy.sh"
 CONFIG_FILE=$(realpath "${1}")                        # 传入的配置文件
-ALON_PATH=$(dirname "${CONFIG_FILE}")                 # 配置Alon源路径
+ALON_PATH=$("${GITHUB_WORKSPACE}")                    # 配置Alon源路径
 CONFIG_PATH=$(dirname "${CONFIG_FILE}")               # 配置文件路径
 CONFIG_NAME=$(basename "${CONFIG_FILE}" .config)      # 配置文件名
 IFS=';' read -r -a CONFIG_ARRAY <<< "${CONFIG_NAME}"  # 分割配置文件名
